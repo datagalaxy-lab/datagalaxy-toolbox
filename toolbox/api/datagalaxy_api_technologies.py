b8d529babb063bf5fd4b2357448077cde5ba2a57
Etienne Scholly <etienne.scholly@datagalaxy.com>
2023-11-03 16:41:09 +0100

    API: implement methods to get/post
    - list_technologies retrieves all technologies from a clientspace
    - insert_technology creates a new technology in the clientspace

---
+++ toolbox/api/datagalaxy_api_technologies.py
@@ -0,0 +1,29 @@
+import logging
+import requests as requests
+
+
+class DataGalaxyApiTechnology:
+    def __init__(self, url: str, access_token: str):
+        self.url = url
+        self.access_token = access_token
+
+    def list_technologies(self) -> list:
+        headers = {'Authorization': f"Bearer {self.access_token}"}
+        response = requests.get(f"{self.url}/technologies", headers=headers)
+        code = response.status_code
+        body_json = response.json()
+        if code != 200:
+            raise Exception(body_json['error'])
+
+        logging.info(f'list_technologies - {len(body_json["technologies"])} technologies found')
+        result = body_json['technologies']
+        return result
+
+    def insert_technology(self, technology) -> object:
+        headers = {'Authorization': f"Bearer {self.access_token}"}
+        response = requests.post(f"{self.url}/technologies", json=technology, headers=headers)
+        code = response.status_code
+        body_json = response.json()
+        if code != 201:
+            raise Exception(body_json['error'])
+        return body_json
